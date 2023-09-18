82882e1ccb064b069f7ecbc7e7294ca15f5080d8
Tilen Vatovec <tilen.vatovec@gmail.com>
2023-09-18 20:12:51 +0200

    Get user location

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -14,30 +14,77 @@ export class SearchComponent implements OnInit, AfterViewInit {
 
   @Input() placeholder = ""
 
-  @Output() locationChanged: EventEmitter<{lat: number, lng: number, placeID: string | undefined}> = new EventEmitter();
+  @Output() locationChanged: EventEmitter<{lat: number, lng: number, city: string | undefined}> = new EventEmitter();
 
   autocomplete: google.maps.places.Autocomplete | undefined
 
   constructor() {}
 
   ngOnInit(): void {
-    console.log(2)
+    // Get user's location
+    this.getUserLocation()
+      .then(location => {
+        const lat = location.lat;
+        const lng = location.lng;
+
+        // Update inputField
+        this.inputField.nativeElement.value = `${lat}, ${lng}`;
+
+        // Update autocomplete
+        if (this.autocomplete) {
+          this.autocomplete.setBounds(new google.maps.LatLngBounds(location));
+        }
+      })
+      .catch(error => {
+        console.error("Failed to get user location:", error);
+      });
   }
+
   ngAfterViewInit() {
     this.autocomplete = new google.maps.places.Autocomplete(this.inputField.nativeElement);
 
     this.autocomplete.addListener('place_changed', () => {
       const place = this.autocomplete?.getPlace();
-      if (place && place.geometry && place.geometry.location) {
+      if (place && place.geometry && place.geometry.location && place.address_components) {
         const lat = place.geometry.location.lat();
         const lng = place.geometry.location.lng();
-        const placeID = place?.place_id
-        this.locationChanged.emit({ lat, lng, placeID});
-        console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+
+        let city = "";
+        for (let i = 0; i < place.address_components.length; i++) {
+          let addressType = place.address_components[i].types[0];
+          if (addressType === "locality") {
+            city = place.address_components[i]["long_name"];
+            break;
+          }
+        }
+
+        this.locationChanged.emit({ lat, lng, city });
       }
     });
 
   }
+
+  getUserLocation(): Promise<{ lat: number, lng: number }> {
+    return new Promise((resolve, reject) => {
+       if (navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(
+           position => {
+            console.log(position)
+             const lat = position.coords.latitude;
+             const lng = position.coords.longitude;
+             resolve({ lat, lng });
+           },
+           error => {
+             // Handle error
+             reject(error);
+           }
+         );
+       } else {
+         reject("Geolocation is not supported by this browser.");
+       }
+    });
+  }
+
 
 
 }
