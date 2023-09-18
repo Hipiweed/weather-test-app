c581897bbd97e495a6bc4e0a52aaddf87c770a33
Tilen Vatovec <tilen.vatovec@gmail.com>
2023-09-18 20:11:29 +0200

    Pass data to app-weather

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/WeatherDataModel';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +10,27 @@ import { WeatherService } from './services/weather.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  weatherData:undefined | WeatherData[] = undefined;
 
-  constructor(private weatherService: WeatherService) {}
+  constructor(private weatherService: WeatherService, private changeDetector: ChangeDetectorRef) {}
 
-  onLocationChanged(location: {lat: number, lng: number, placeID: string | undefined}) {
+  onLocationChanged(location: {lat: number, lng: number, city: string | undefined}) {
     const lat = location.lat.toString();
     const lng = location.lng.toString();
-    const placeID = location.placeID
-    if(!placeID) throw Error('No place id cannot get data')
-    this.weatherService.getWeatherDataForThisWeak(lat, lng, placeID).subscribe(data => {
-      console.log(data)
-  })
+    this.weatherService.getWeatherDataForThisWeak(lat, lng).subscribe((data: any) => {
+      const weatherData: WeatherData[] = data.daily.time.map((time: string, index: number) => {
+        return {
+          maxTemp: data.daily.temperature_2m_max[index],
+          minTemp: data.daily.temperature_2m_min[index],
+          rain: data.daily.rain_sum[index],
+          avrageTemp: (data.daily.temperature_2m_max[index] + data.daily.temperature_2m_min[index]) / 2,
+          city:location.city,
+          windSpeed: data.daily.windspeed_10m_max[index]
+        };
+      });
+      this.weatherData = weatherData
+      this.changeDetector.detectChanges();
+    })
 
   }
 
